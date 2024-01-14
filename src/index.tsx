287788121209c47924de38dde457ac472e10871c
onlyheartt8 <422474821@qq.com>
2024-01-15 01:30:38 +0800

    ✨ feat:
    add playground components export

--- src/index.tsx
+++ src/index.tsx
@@ -2,3 +2,4 @@ export * from './components/Console/Console.tsx';
 export * from './components/Editor/Editor.tsx';
 export * from './components/Toolbar/Toolbar.tsx';
 export * from './components/Layout/Layout.tsx';
+export * from './components/Playground/Playground.tsx';
